32d528255ec1f524c9437d96c138fe952039de16
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-23 23:35:15 -0700

    Add another endpoint

--- server.js
+++ server.js
@@ -6,3 +6,19 @@ app.use(express.static('public'));
 app.listen(3000, () => {
   console.log('Running on localhost:3000');
 });
+
+app.get('/api/v1/projects', (request, response) => {
+  // get projects
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  // post new project
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  // post new palette
+});
+
+app.delete('/api/v1/palettes', (request, response) => {
+  // delete palette
+});
