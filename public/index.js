961c0afabca17c3c5a5e1295feeb5d0cc59c52e9
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-23 22:44:27 -0700

    Write functionality for assigning random colors to palette

---
+++ public/index.js
@@ -0,0 +1,27 @@
+// EVENT LISTENERS
+$('.new-colors').click(changeColors)
+
+// fn to make random hex colors // returns 6 digit hex code
+function getRandomColor() {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+// when new colors btn is clicked, run gRC() five times and assign each color to each div
+// also assign to background...?
+function changeColors() {
+  const firstColor = getRandomColor();
+  const secondColor = getRandomColor();
+  const thirdColor = getRandomColor();
+  const fourthColor = getRandomColor();
+  const fifthColor = getRandomColor();
+
+  assignColors(firstColor, secondColor, thirdColor, fourthColor, fifthColor)
+}
+
+function assignColors(color1, color2, color3, color4, color5) {
+  $('.first').css('background-color', `#${color1}`);
+  $('.second').css('background-color', `#${color2}`);
+  $('.third').css('background-color', `#${color3}`);
+  $('.fourth').css('background-color', `#${color4}`);
+  $('.fifth').css('background-color', `#${color5}`);
+}
