88bbe24f30e1a99a2a37590046f6bd96f38473a1
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-24 14:52:54 -0700

    Add initial migrations

---
+++ db/seeds/dev/projects.js
@@ -0,0 +1,21 @@
+
+exports.seed = function(knex, Promise) {
+  
+};
+
+// Clear out both tables (palettes first, as they depend on projects existing)
+
+// Add a project => return the project’s id => add palettes with that project id
+
+// Add a project => return the project’s id => add palettes with that project id
+
+// // Deletes ALL existing entries
+//   return knex('table_name').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('table_name').insert([
+//         {id: 1, colName: 'rowValue1'},
+//         {id: 2, colName: 'rowValue2'},
+//         {id: 3, colName: 'rowValue3'}
+//       ]);
+//     });
