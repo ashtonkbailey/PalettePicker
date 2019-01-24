68e63530b487fdb804620fb4474e2862aedfaacf
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-24 16:23:43 -0700

    Add packages and set up to connect knex

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
@@ -17,6 +22,10 @@ app.get('/api/v1/projects', (request, response) => {
 app.post('/api/v1/projects', (request, response) => {
   // post new project
 });
+
+app.get('/api/v1/palettes', (request, response) => {
+  // get palettes
+})
 
 app.post('/api/v1/palettes', (request, response) => {
   // post new palette
