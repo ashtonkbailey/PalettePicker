4bd0ded75bcaad677b42365c0c52260d84b7b938
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-24 17:44:31 -0700

    Write endpoint to get all palettes

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ app.post('/api/v1/projects', (request, response) => {
 });
 
 app.get('/api/v1/palettes', (request, response) => {
-  // get palettes
+  database('palettes').select()
+    .then((palettes) => {
+      response.status(200).json(palettes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    })
 })
 
 app.post('/api/v1/palettes', (request, response) => {
